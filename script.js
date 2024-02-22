9746b43bc886679089e1652714d72ec7fff7a66b
amso95 <71275060+amso95@users.noreply.github.com>
2024-02-22 10:02:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const x = 5
 const y = 7
 function double (x) { return x * 2 }
-console.print('Dubbelt av 5 och 7 är ' + ( double(x) + double(y)))
+console.print('Dubbelt av 5 och 7 är ' + (double(x) + double(y)))
